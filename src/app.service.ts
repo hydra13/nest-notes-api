ba3b717e70864318215a6ff43d607826ce9b7bc5
hydra13 <rob.sabirov@yandex.ru>
2019-10-09 21:33:13 +0300

    added description in app API

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInfo(): string {
+    return `
+    Welcome to my simple notes project!
+    
+    API:
+    1) Get all notes:
+    GET /notes 
+
+    2) Get note by ID
+    GET /notes/:id 
+    
+    3) Create new note:
+    POST /notes 
+    {
+      title: :title,
+      text: :text
+    }
+    
+    4) Update note by ID:
+    PUT /notes/:id
+    {
+      title: :title,
+      text: :text
+    }
+    
+    5) Delete note by ID:
+    DELETE /notes/:id`;
   }
 }
